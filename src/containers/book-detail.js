137faa64a512d54015685f31ba965a93f83d5bad
Daniel Luong <daniel.luong@argos.co.uk>
2018-05-28 15:00:36 +0100

    Add extra entity to the array

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -5,12 +5,13 @@ class BookDetail extends PureComponent {
   render() {
     if (!this.props.book) return <div>Select a book to get started.</div>
 
-    const { title } = this.props.book
+    const { title, pages } = this.props.book
 
     return (
       <div>
         <h3>Details for:</h3>
-        <p>{title}</p>
+        <p>Title: {title}</p>
+        <p>Pages: {pages}</p>
       </div>
     )
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Dragonball Z'},
-    {title: 'Tina Luong'}
+    {title: 'Javascript: The Good Parts', pages: 23},
+    {title: 'Harry Potter', pages: 43},
+    {title: 'Dragonball Z', pages: 1},
+    {title: 'Tina Luong', pages: 7}
   ]
 }
